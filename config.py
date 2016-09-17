8b2a1199d98e2e16b86a0695badebb1050bea9b4
MaxLager <maksmesss@gmail.com>
2016-09-17 21:06:01 +0200

    Main commit
    bot.py
    config.py
    acrcloud dir
    LICENSE
    README
    requirements.txt

---
+++ config.py
@@ -0,0 +1,28 @@
+'''Config.py stores configuration data
+token - Telegram Bot token
+photo_id_X - stores photo id's'''
+token = 'TOKEN'
+
+
+cfg = {
+    'host':  'eu-west-1.api.acrcloud.com',
+    'access_key': 'ACCESS_KEY',
+    'access_secret': 'ACCESS_SECRET',
+    'debug': False,
+    'timeout': 10
+    }
+
+
+audio_dir = '/home/.../TadamBot/audios/' #was used to store bad requests in BETA
+db_dir    = '/home/.../TadamBot/data/'
+
+
+deezer_link  = 'http://www.deezer.com/track/'
+spotify_link = 'https://open.spotify.com/track/'
+yt_link      = 'https://www.youtube.com/watch?v='
+tadamweb     = 'http://tadambot.com'
+
+
+photo_id_d = 'AgADAgADz6cxG8WMWAUskMrxTt22OvgSSA0ABHgZzMS6qmlSaC0BAAEC'
+photo_id_s = 'AgADAgADzqcxG8WMWAUdqIgh4uQvhIYCSA0ABIxQomHyg853Li0BAAEC'
+
